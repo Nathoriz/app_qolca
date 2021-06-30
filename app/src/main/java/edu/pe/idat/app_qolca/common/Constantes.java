47c572a095f2a82093c815ac7ed972c88798649b
key <dkeykoc.2603@gmail.com>
2021-06-29 21:05:47 -0500

    creacion de registro

--- app/src/main/java/edu/pe/idat/app_qolca/common/Constantes.java
+++ app/src/main/java/edu/pe/idat/app_qolca/common/Constantes.java
@@ -2,7 +2,7 @@ package edu.pe.idat.app_qolca.common;
 
 public class Constantes {
     private static String ipv="192.168.1.3";
-    public static final String URL_API_USUARIO_CREAR="http://"+ipv+":8050/usuario/crearCuenta";
+    public static final String URL_API_USUARIO_CREAR="http://"+ipv+":8050/usuario/signup";
     public static final String URL_API_USUARIO_LOGIN="http://"+ipv+":8050/usuario/login";
 
 
--- app/src/main/java/edu/pe/idat/app_qolca/view/SignUpActivity.java
+++ app/src/main/java/edu/pe/idat/app_qolca/view/SignUpActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.widget.Toast;
 
 
@@ -19,6 +20,7 @@ import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 import edu.pe.idat.app_qolca.common.Constantes;
 import edu.pe.idat.app_qolca.databinding.ActivitySignUpBinding;
@@ -30,7 +32,6 @@ public class SignUpActivity extends AppCompatActivity {
     public SignUpActivity() {
     }
 
-
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,23 +40,63 @@ public class SignUpActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
         getSupportActionBar().hide();
 
-        binding.btnLogin.setOnClickListener(view -> {
+        binding.btnRcrear.setOnClickListener(view -> {
+
 
             if(registrarUsuario(Constantes.URL_API_USUARIO_CREAR)){
                 mostrarAlerta("Registro","Usuario creado exitoso");
             }
 
-        });
+                String msj = "";
+                boolean respuesta = true;
+                if (binding.etSignupNombre.getText().toString().length() == 0) {
+                    msj = "Ingrese el nombre";
+                    respuesta = false;
+                    mostrarAlerta("Nombre", msj);
 
+                } else if (binding.etSignupApellido.getText().toString().length() == 0) {
+                    msj = "Ingrese su apellido";
+                    respuesta = false;
+                    mostrarAlerta("Apellido", msj);
+                } else if (binding.etSignupEmail.getText().toString().length() == 0) {
+                    msj = "Ingrese su email";
+                    respuesta = false;
+                    mostrarAlerta("Email", msj);
+                } else if (binding.etSignupEmail.getText().toString().length() > 0) {
+                    Pattern pattern = Patterns.EMAIL_ADDRESS;
+                    if (pattern.matcher(binding.etSignupEmail.getText().toString()).matches()) {
+                        respuesta = true;
+                    } else {
+                        msj = "Su email " + binding.etSignupEmail.getText().toString() + " no es valido";
+                        respuesta = false;
+                        mostrarAlerta("correo", msj);
+                    }
+                } else if (binding.etSignupContrasena.getText().toString().length() == 0) {
+                    msj = "Ingrese su correo";
+                    respuesta = false;
+                    mostrarAlerta("Email", msj);
+                } else if (binding.etSignupContrasena.getText().toString().length() > 0) {
+                    Pattern pattern = Patterns.EMAIL_ADDRESS;
+                    if (pattern.matcher(binding.etSignupContrasena.getText().toString()).matches()) {
+                        respuesta = true;
+                    } else {
+                        msj = "Su contraseña" + binding.etSignupContrasena.getText().toString() + " no es valido";
+                        respuesta = false;
+                        mostrarAlerta("contraseña", msj);
+                    }
+                } else {
+                    respuesta = true;
+                }
+
+
+
+
+
+        });
 
         binding.tvSignupLogin.setOnClickListener(view -> {
             startActivity(new Intent(SignUpActivity.this,LoginActivity.class));
-
-
         });
-
-
-
     }
     public void mostrarAlerta(String titulo, String mensaje) {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
@@ -69,14 +110,13 @@ public class SignUpActivity extends AppCompatActivity {
     private boolean registrarUsuario(String urlApiUsuarioCrear) {
         RequestQueue colapeticiones = Volley.newRequestQueue(this);
         boolean rsp=false;
-        binding.btnLogin.setEnabled(false);
+        binding.btnRcrear.setEnabled(false);
 
-        Map<String,String> parametros= new HashMap<>();
+       Map<String,String> parametros= new HashMap<>();
+       parametros.put("nombre",binding.etSignupNombre.getText().toString());
         parametros.put("apellido",binding.etSignupApellido.getText().toString());
-        parametros.put("contrasenia",binding.etSignupContrasena.getText().toString());
-
         parametros.put("email",binding.etSignupEmail.getText().toString());
-        parametros.put("nombre",binding.etSignupNombre.getText().toString());
+        parametros.put("contrasenia",binding.etSignupContrasena.getText().toString());
 
         rsp=true;
         JSONObject jsonObjectParametro = new JSONObject(parametros);
@@ -94,10 +134,15 @@ public class SignUpActivity extends AppCompatActivity {
             public void onErrorResponse(VolleyError error) {
 
             }
+
+
+
         });
         colapeticiones.add(request);
         return rsp;
     }
+
+
 
     private void mensaje(String m){
         Toast.makeText(getApplicationContext(),
